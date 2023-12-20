3da0ed2907dd2e08f6d73e72c476752cef65e83d
Chinwe Chukwuogor <chukwuogorchinwe@gmail.com>
2023-12-20 16:51:41 +0100

    Seperating middleware into different files

--- index.js
+++ index.js
@@ -2,28 +2,11 @@ const config = require("./utils/config")
 const logger = require("./utils/logger")
 const express = require("express")
 const app = express()
-const Blog = require("./models/blog")
+const blogRouter = require('./controllers/bloglist')
 
 
 app.use(express.json())
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
-  logger.info('Sent')
-})
-
-app.get("/api/blogs", (req, res) => {
-  Blog.find({}).then(blogs => {
-    res.json(blogs)
-  })
-})
-
-app.post("/api/blogs/", (req, res) => {
-  const blog = new Blog(req.body)
-  blog.save()
-    .then(result => {
-    res.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server is currently running on port: ${config.PORT} for ${config.MONGODB_URI}`)
